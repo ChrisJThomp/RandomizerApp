980c74540786f811d813f9affc9c532a76b6c2de
ChrisJThomp <thompson.j.christian@gmail.com>
2020-12-11 14:44:04 -0500

    first commit

---
+++ Randomizer/RandomizerApp.swift
@@ -0,0 +1,31 @@
+//
+//  RandomizerApp.swift
+//  Randomizer
+//
+//  Created by Josh Hubbard on 12/11/20.
+//
+
+import SwiftUI
+
+@main
+struct RandomizerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {var body: some View {
+        VStack() {
+            HStack() {
+                // textfield and save/load button at top
+            }
+            VStack() {
+                // tableview and label above enter button
+            }
+            VStack() {
+                // enter button
+                HStack() {
+                    // choose buttons
+                }
+                // clear button
+            }
+        }
+      }
+}
---
+++ RandomizerApp.swift
@@ -0,0 +1,13 @@
+//
+//  RandomizerApp.swift
+//  Randomizer
+//
+//  Created by Christian Thompson on 12/9/20.
+//
+
+import SwiftUI
+struct AwesomeView: View {
+  var body: some View {
+    Text("Hey! This is aawweesomee!")
+  }
+}
